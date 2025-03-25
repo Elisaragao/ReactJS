5776d444c6d1047e88a645b16edaf7fed4fbeb7f
Elisaragao <elisangelaaragao644@gmail.com>
2025-03-25 13:50:48 -0300

    Ciclo de vida

--- src/App.jsx
+++ src/App.jsx
@@ -5,46 +5,29 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      nome: 'Elisangela',
-      contador: 0
+      hora: '00:00:00'
     }
-
-    this.aumentar = this.aumentar.bind(this)
-    this.diminuir = this.diminuir.bind(this)
   }
 
-
-
-  aumentar() {
-    let state = this.state
-    state.contador += 1
-    this.setState(state)
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({ hora: new Date().toLocaleTimeString() })
+    }, 1000)
   }
 
-  diminuir() {
-    let state = this.state
-    if(state.contador === 0){
-      alert('Ops, já chegou em zero')
-      return //Pra parar por aqui
-    }
-
-    state.contador -= 1
-    this.setState(state)
+  componentDidUpdate() {
+    console.log("Atualizou!")
   }
+
 
   render() {
     return (
       <>
-        <h1>Contador</h1>
-        {this.state.nome}
-        <h2>
-          <button onClick={this.diminuir}>-</button>
-          {this.state.contador}
-          <button onClick={this.aumentar}>+</button>
-        </h2>
+        <h1>Meu projeto {this.state.hora}</h1>
       </>
     )
   }
+
 }
 
 export default App
